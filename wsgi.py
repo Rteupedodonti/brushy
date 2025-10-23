5f10b818b23d48c48b8cab928e844264b893e8bc
Rteupedodonti <gamzesumeyye.avsar@erdogan.edu.tr>
2025-10-24 01:19:28 +0300

    Add WSGI application setup in wsgi.py
    Initialize the WSGI application using create_app.

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from app import create_app
+
+# uWSGI sunucusunun bulacağı "app" objesini oluşturur
+app = create_app()
+
+# Not: Diğer satırları (örneğin if __name__ == "__main__") bu dosyaya eklemeyin.
